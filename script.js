58bacbbc02286e041825f69b33f14861b9be0e17
Ernie <ernestjohnson80@yahoo.com>
2021-04-24 20:53:24 -0700

    first comm

---
+++ script.js
@@ -0,0 +1,268 @@
+var d = new Date();
+var n = d.getDay();
+
+var daySpan = 7;
+var daysBack = n - 1;
+
+var days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+var daysFull = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
+var months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+var dat = new Date();
+
+Date.prototype.addDays = function (days) {
+  var dat = new Date(this.valueOf());
+  dat.setDate(dat.getDate() - daysBack + days);
+  return dat;
+};
+Date.prototype.getDayName = function () {
+  return days[this.getDay()];
+};
+Date.prototype.getDayNameFull = function () {
+  return daysFull[this.getDay()];
+};
+Date.prototype.getMonthName = function () {
+  return months[this.getMonth()];
+};
+
+function pad(input) {
+  var BASE = '00';
+  return input ? BASE.substr(0, 2 - Math.ceil(input / 9)) + input : BASE;
+}
+
+$('.currentMonth').text(dat.getMonthName());
+$('.currentYear').text(dat.getFullYear());
+$('.currentDay').text(dat.getDayNameFull());
+
+setTime();
+
+function setTime() {
+  var dat2 = new Date();
+  var seconds = dat2.getSeconds();
+  var hours = dat2.getHours();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+
+  if (hours > 12) {
+    hours -= 12;
+  } else if (hours === 0) {
+    hours = 12;
+  }
+
+  $('.hour').text(pad(hours));
+  $('.minute').text(pad(dat2.getMinutes()));
+  $('.second').text(pad(seconds));
+  $('.meaning').text(ampm);
+
+  var int = (60 - dat2.getSeconds()) * 1000;
+
+  setTimeout(setTime, int);
+}
+
+var dd = 100 / daySpan;
+
+$(function () {
+  $('.dateHolder li').width(dd + '%');
+  $('.dateTimeWrapper').css('padding-left', dd / 2 + '%');
+});
+
+for (i = 0; i < daySpan; i++) {
+  var nextDate = dat.addDays(i);
+
+  var strDate = '<div class ="date">' + pad(nextDate.getDate()) + '</div>';
+  var strDay = '<div class ="day" >' + nextDate.getDayName() + '</div>';
+
+  if (nextDate.toISOString().slice(0, 10) == dat.toISOString().slice(0, 10)) {
+    strDay = '<div class ="day dateCurrent">' + nextDate.getDayName() + '</div>';
+    strDate = '<div class ="date dateCurrent">' + nextDate.getDate() + '</div>';
+  }
+
+  $('.dateHolder').append('<li>' + strDay + strDate + '</li>');
+}
+
+var numberOfVirtualAnts = 4000; // Seems to work fine with up to a good few thousand more than this, although it can be a fair bit slower
+var colors = ['#DDDDDD', 'black']; // Must be two colors
+
+var gridWidth, gridHeight;
+var grid = {};
+
+// Vant for 'virtual ant'
+function Vant(x, y, orientation, rules) {
+  this.x = x;
+  this.y = y;
+  this.orientation = orientation;
+  this.state = 0;
+  this.color = 0;
+  // 1 = noturn, 2 = right, 4 = uturn, 8 = left
+  // Rules come in tuples: [new color, direction to turn, new state]
+  this.rules = rules;
+}
+
+var vants = [];
+
+function randBetween(min, max) {
+  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
+}
+
+function randomRule() {
+  return [randBetween(0, 1), [1, 2, 4, 8][randBetween(0, 3)], randBetween(0, 1)];
+}
+
+function randomRuleset() {
+  return [
+    [randomRule(), randomRule()],
+    [randomRule(), randomRule()],
+  ];
+}
+
+// this kind of turmite uses relative positioning -- the ant has its own internal orientation. this function handles turning the ant in the appropriate direction -- 'right' if it is currently on a white square, 'left' if it is currently on a black square.
+function findOrientation(dir, ori) {
+  // dirs: 1 = noturn, 2 = right, 4 = uturn, 8 = left
+  if (dir === 1) {
+    // noturn
+    return ori;
+  } else if (dir === 4) {
+    // uturn
+    if (ori === 'n') {
+      return 's';
+    } else if (ori === 's') {
+      return 'n';
+    } else if (ori === 'e') {
+      return 'w';
+    } else {
+      return 'e';
+    }
+  } else {
+    // right (2) or left (8)
+    if (ori == 'n') {
+      return dir === 2 ? 'e' : 'w';
+    } else if (ori == 'e') {
+      return dir === 2 ? 's' : 'n';
+    } else if (ori == 's') {
+      return dir === 2 ? 'w' : 'e';
+    } else {
+      // 'w'
+      return dir === 2 ? 'n' : 's';
+    }
+  }
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  gridWidth = windowWidth;
+  gridHeight = windowHeight;
+
+  function randomX() {
+    return Math.ceil(Math.random() * gridWidth);
+  }
+  function randomY() {
+    return Math.ceil(Math.random() * gridHeight);
+  }
+
+  // rules tuples: [new color (0/1), direction to turn (1 / 2 / 4 / 8), new state (0 / 1)]
+  var vant1 = new Vant(Math.floor(gridWidth / 2), Math.floor(gridHeight / 2), 'n', [
+    [
+      [1, 2, 0],
+      [0, 8, 0],
+    ],
+    [
+      [1, 1, 0],
+      [1, 1, 1],
+    ],
+  ]); // x, y, orientation, state
+  var vant2 = new Vant(Math.floor(gridWidth / 2 + 50), Math.floor(gridHeight / 2), 'e', [
+    [
+      [1, 1, 1],
+      [1, 8, 0],
+    ],
+    [
+      [1, 2, 1],
+      [0, 1, 0],
+    ],
+  ]);
+  var vant3 = new Vant(Math.ceil(Math.random() * gridWidth), Math.ceil(Math.random() * gridHeight), 'w', [
+    [
+      [1, 4, 1],
+      [1, 8, 0],
+    ],
+    [
+      [1, 2, 1],
+      [0, 1, 0],
+    ],
+  ]);
+
+  vants.push(vant1, vant2, vant3);
+
+  for (var i = 0; i <= numberOfVirtualAnts; i++) {
+    vants.push(new Vant(randomX(), randomY(), ['n', 'e', 's', 'w'][randBetween(0, 3)], randomRuleset()));
+  }
+}
+
+function draw() {
+  vants.forEach((vant) => {
+    // Does the current square have a value?
+    if (typeof grid[vant.x] == 'object' && typeof grid[vant.x][vant.y] == 'number') {
+      // If so, get that value from the grid storage object.
+      vant.color = grid[vant.x][vant.y];
+    } else {
+      // otherwise, treat that value as false.
+      vant.color = 0;
+    }
+
+    var rule = vant.rules[vant.state][vant.color];
+    vant.orientation = findOrientation(rule[1], vant.orientation);
+    stroke(colors[rule[0]]);
+    vant.color = rule[0];
+    vant.state = rule[2];
+    point(vant.x, vant.y);
+
+    if (typeof grid[vant.x] == 'object') {
+      grid[vant.x][vant.y] = vant.color;
+    } else {
+      // If there's no data for this row yet, make that row first, and then add the new value.
+      grid[vant.x] = {};
+      grid[vant.x][vant.y] = vant.color;
+    }
+
+    // Move the ant in the appropriate direction for the next iteration.
+    if (vant.orientation === 'n') {
+      vant.y -= 1;
+    } else if (vant.orientation === 's') {
+      vant.y += 1;
+    } else if (vant.orientation === 'e') {
+      vant.x += 1;
+    } else {
+      vant.x -= 1;
+    }
+    if (vant.x > windowWidth) {
+      vant.x = 0;
+    }
+    if (vant.x < 0) {
+      vant.x = windowWidth;
+    }
+    if (vant.y > windowHeight) {
+      vant.y = 0;
+    }
+    if (vant.y < 0) {
+      vant.y = windowHeight;
+    }
+
+    stroke('white');
+    point(vant.x, vant.y);
+  });
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
